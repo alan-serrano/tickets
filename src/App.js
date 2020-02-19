b0d92c0b8d09e9dfc779e3e9a77ba267ff9fab65
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-19 15:57:04 -0400

    Adding static components to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import './App.scss';
+import Form from './components/form';
+import Wrapper from './components/wrapper';
+import Ticket from './components/ticket';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Ticket Generator</h1>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+
+        return (
+            <div className="App">
+                <h1 className="text-center">Ticket Generator</h1>
+                <Form />
+                <Wrapper>
+                    <Ticket
+                        logo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W"
+                        title = "TITLE"
+                        description = "Here is the ticket description"
+                        date = "01.01.2020"
+                        code = "000000"
+                    />
+                    <Ticket
+                        logo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W"
+                        title = "TITLE"
+                        description = "Here is the ticket description"
+                        date = "01.01.2020"
+                        code = "000000"
+                    />
+                    <Ticket
+                        logo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W"
+                        title = "TITLE"
+                        description = "Here is the ticket description"
+                        date = "01.01.2020"
+                        code = "000000"
+                    />
+                    <Ticket
+                        logo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W"
+                        title = "TITLE"
+                        description = "Here is the ticket description"
+                        date = "01.01.2020"
+                        code = "000000"
+                    />
+                    <Ticket
+                        logo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W"
+                        title = "TITLE"
+                        description = "Here is the ticket description"
+                        date = "01.01.2020"
+                        code = "000000"
+                    />
+                    <Ticket
+                        logo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W"
+                        title = "TITLE"
+                        description = "Here is the ticket description"
+                        date = "01.01.2020"
+                        code = "000000"
+                    />
+                </Wrapper>
+            </div>
+        );
+    }
+
 }
 
 export default App;

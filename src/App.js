f691d85a16075c2316d234605e265e3c82fce41e
alan-serrano <alan.serrano.pch@gmail.com>
2020-02-22 20:27:08 -0400

    Fix print view, and number generation
    PRINT VIEW
    - Changes in style App.scss
    - Added component wrapper in loop "generateTickets" on App.js
    
    NUMBER GENERATION
    - Formatting code in each iteration of "generateTickest" on App.js and added as prop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.scss';
-import Form from './components/form';
-import Wrapper from './components/wrapper';
-import Ticket from './components/ticket';
+import Form from './components/Form';
+import Wrapper from './components/Wrapper';
+import Ticket from './components/Ticket';
 
 class App extends React.Component {
 
@@ -12,8 +12,7 @@ class App extends React.Component {
         inputDate: '',
         inputQuantity: 0,
         inputDescription: '',
-        code: 0,
-        quantity: 1,
+        quantity: 0,
         textarea: ''
     }
 
@@ -27,38 +26,59 @@ class App extends React.Component {
         this.setState({quantity: quantity})
     }
 
-    tickets() {
-        var tmp = [];
-
+    generateTickets() {
+        var wrappers = [];
+        var counter = 1;
+        
         // If quantity is empty
         if( !this.state.quantity ) {
-            tmp.push(
-                <Ticket 
+
+            wrappers.push(
+                <Wrapper key="0">
+                    <Ticket 
                     logo_url={this.state.inputLogoUrl || 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W'}
                     title = {this.state.inputTitle || 'TITLE'}
                     description = {this.state.textarea || 'Here is the ticket description'}
                     date = {this.state.inputDate || '01.01.2020'}
-                    code = {this.state.code || '0000'}
-                />
+                    code = {'0001'}
+                    />
+                </Wrapper>              
             );
 
         } else {
-            for (let i = 0; i < this.state.quantity; i++) {
-                tmp.push(
-                    <Ticket 
-                        logo_url={this.state.inputLogoUrl || 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W'}
-                        title = {this.state.inputTitle || 'TITLE'}
-                        description = {this.state.textarea || 'Here is the ticket description'}
-                        date = {this.state.inputDate || '01.01.2020'}
-                        code = {this.state.code || '0000'}
-                        key={i}
-                    />
-                );
+            for (let i = 0; i < this.state.quantity / 6; i++) {
+                let tickets = []; // Empty array that store the tickets generated
+
+                for (let j = 0; j < 6 && counter <= this.state.quantity; j++) {
+                    // Formating code
+                    let code = '0000';
+                    let tmpString = '' + counter;
+                    code = code.slice(tmpString.length) + tmpString;
+                    //--------------
+
+                    tickets.push(
+                        <Ticket 
+                            logo_url={this.state.inputLogoUrl || 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDK5X2Sjws2k1i6rKJHnO1mnDBtg2AFruxmBjZHbvATgc1b8_W'}
+                            title = {this.state.inputTitle || 'TITLE'}
+                            description = {this.state.textarea || 'Here is the ticket description'}
+                            date = {this.state.inputDate || '01.01.2020'}
+                            code = {code || '0000'}
+                            key={counter}
+                        />
+                    );
+
+                    counter++;
+                }
+
+                wrappers.push(
+                    <Wrapper key={i}>
+                        {tickets}
+                    </Wrapper>
+                )
             }
         }
 
-
-        return tmp
+        return wrappers
     }   
 
     render() {
@@ -69,9 +89,9 @@ class App extends React.Component {
                     onInputsChange = {this.onInputsChange}
                     onFormSubmit = {this.onFormSubmit}    
                 />
-                <Wrapper>
-                    {this.tickets()}
-                </Wrapper>
+                <section className="tickets">
+                    {this.generateTickets()}
+                </section>
             </div>
         );
     }
